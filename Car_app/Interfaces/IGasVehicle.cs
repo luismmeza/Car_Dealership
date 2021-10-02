cbef32f7615c8d5b966cbb738a9764fbb37ea823
luismmeza <luismmeza21@gmail.com>
2021-10-01 21:46:22 -0500

    created GasCar Class

---
+++ Car_app/Interfaces/IGasVehicle.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Car_app.Interfaces
+{
+    public interface IGasVehicle : IMotorVehicle
+    {
+        public string OilType { get; set; }
+        public string GasType { get; set; }
+        public int MPG { get; set; }
+        
+    }
+}
---
+++ Car_app/VehicleCLasses/GasCar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Car_app.Interfaces;
+
+namespace Car_app.VehicleCLasses
+{
+    public class GasCar : IGasVehicle
+    {
+        public string Body_Type { get; set; }
+        public string OilType { get; set; }
+        public string GasType { get ; set ; }
+        public int MPG { get ; set ; }
+        public string HeadlightType { get ; set ; }
+        public int Mileage { get ; set; }
+        public string Drivetrain { get ; set ; }
+        public string Engine { get ; set; }
+        public string Color { get ; set ; }
+        public string Manufacturer { get ; set ; }
+
+        public GasCar()
+        { }
+        
+    }
+}
