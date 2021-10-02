8cc01a48f49e0e85ca1410b3f99804f43a35ca42
victor-meza-07 <victord.meza@gmail.com>
2021-10-01 21:22:29 -0500

    added the IMotorVehicle interface

---
+++ Car_app/Interfaces/IMotorVehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Car_app.Interfaces
+{
+    public interface IMotorVehicle
+    {
+        public string HeadlightType { get; set; }
+        public int Mileage { get; set; }
+        public string Drivetrain { get; set; }
+        public string Engine { get; set; }
+        public string Color { get; set; }
+        public string Manufacturer { get; set; }
+
+    }
+}
