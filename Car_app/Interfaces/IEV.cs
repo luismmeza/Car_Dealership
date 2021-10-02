a5bb8641ce777d7de41d491861de5c57c2760cea
victor-meza-07 <victord.meza@gmail.com>
2021-10-01 21:39:26 -0500

    added the IEV interface

---
+++ Car_app/Interfaces/IEV.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Car_app.Interfaces
+{
+    public interface IEV
+    {
+        public int MilesPerCharge { get; set; }
+        public int MinutesToCharge { get; set; }
+    }
+}
